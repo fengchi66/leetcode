5f87f39a4f73eddeb5fe997777599ab114f9d068
wufengchi <wufengchi@yit.com>
2021-08-14 19:56:52 +0800

    init

--- src/main/java/offer02/Solution46.java
+++ src/main/java/offer02/Solution46.java
@@ -2,18 +2,21 @@ package offer02;
 
 import bean.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
-// 剑指 Offer II 045. 二叉树最底层最左边的值
-public class Solution45 {
-    public int findBottomLeftValue(TreeNode root) {
+// 剑指 Offer II 046. 二叉树的右侧视图
+public class Solution46 {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+
         Queue<TreeNode> queue1 = new LinkedList<>();
         Queue<TreeNode> queue2 = new LinkedList<>();
 
         queue1.offer(root);
-        // ans表示每一层最左边的值
-        int ans = root.val;
 
         while (!queue1.isEmpty()) {
             TreeNode node = queue1.poll();
@@ -23,17 +26,13 @@ public class Solution45 {
             if (node.right != null)
                 queue2.offer(node.right);
 
-            // 每当遍历完一层时，就将ans更新为该层的第一个值，也就是队首元素
+            // 该层遍历完,此时遍历到的node就该层的最后一个节点
             if (queue1.isEmpty()) {
                 queue1 = queue2;
                 queue2 = new LinkedList<>();
-
-                if (!queue1.isEmpty())
-                    ans = queue1.peek().val;
+                ans.add(node.val);
             }
         }
-
         return ans;
-
     }
 }
